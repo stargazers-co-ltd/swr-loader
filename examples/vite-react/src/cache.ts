d259a9d1bf259b96f4fbd6e1d7b7f6d9c22ae901
freddie <rincofrederico@gmail.com>
2023-09-20 19:56:49 +0100

    chore: convert to monorepo

--- examples/vite-react/src/cache.ts
+++ examples/vite-react/src/cache.ts
@@ -1,8 +1,9 @@
 import { createSWR } from 'swr-loader';
-import { createIDBAdapter } from 'swr-loader/adapters/idb';
+import { createMemoryAdapter } from 'swr-loader/adapters/memory';
 
-const idbAdapter = createIDBAdapter({ dbName: 'demo', storeName: 'data_cache' });
+// const idbAdapter = createIDBAdapter({ dbName: 'demo', storeName: 'data_cache' });
+// const zustandAdapter = createZustandAdapter({ createStore });
 
 export const { swr, invalidate } = createSWR({
-	cacheAdapter: idbAdapter,
+	cacheAdapter: createMemoryAdapter(),
 });
